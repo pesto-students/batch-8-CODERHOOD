a971f9dfb4249889628c8974ce483e4746667672
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-09 17:45:14 +0530

    add babel to server side
    add babel to server side

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
+import express from 'express';
+
 const port = 3000;
 const app = express();
 app.get('/', (req, res) => res.send('Hello from Slack Clone'));
 app.listen(port, () => console.log(`SlackClone Server listening on port ${port}!`));
-module.exports = app;
