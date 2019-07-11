1e302f427c025a110405042933572a4567edc112
Rakesh Tembhurne <rakesh@tembhurne.com>
2019-07-12 00:01:51 +0530

    Add Sidebar component
    Add Sidebar component that can take width as a prop and set
    CSS accordingly.
    
    Refer #19

---
+++ client/src/components/Sidebar/tests/Sidebar.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Sidebar from "../Sidebar";
+
+describe("<Sidebar />", () => {
+  it("should render exactly 1 component", () => {
+    const sidebar = shallow(<Sidebar />);
+    expect(sidebar.children.length).toBe(1);
+  });
+
+  it("should have default column size of 3", () => {
+    const sidebar = shallow(<Sidebar />);
+    expect(sidebar.hasClass('is-3')).toBe(true);
+  });
+
+  it("should be able to set column size", () => {
+    const sidebar = shallow(<Sidebar width="8" />);
+    expect(sidebar.hasClass('is-8')).toBe(true);
+  });
+
+  it("should be able to render childrens", () => {
+    const child = <h1>Dummy Heading</h1>;
+    const sidebar = shallow(<Sidebar>{child}</Sidebar>);
+    expect(sidebar.contains(child)).toBe(true);
+  });
+});
