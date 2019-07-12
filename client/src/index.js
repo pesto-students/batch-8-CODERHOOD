ba6ed3fb47f12656c39763e0cdaa1b7a5f8d2666
kunnu01 <kunalbhatia2409@gmail.com>
2019-07-13 00:28:20 +0530

    add privateRoutes

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,15 @@ import { SignIn, SignUp } from './containers'
 import * as serviceWorker from "./serviceWorker";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { NotFound } from "./components";
+import PrivateRoute from './routes/PrivateRoute';
 
 const routing = (
   <Router>
     <Switch>
-      <Route exact path="/" component={App} />
+      <PrivateRoute exact path="/" component={App} />
       <Route exact path="/signin" component={SignIn} />
-      <Route exact path="/thread" component={Thread} />
-      <Route exact path="/workspace" component={Workspace} />
+      <PrivateRoute exact path="/thread" component={Thread} />
+      <PrivateRoute exact path="/workspace" component={Workspace} />
       <Route exact path="/signup" component={SignUp} />
       <Route component={NotFound} />
     </Switch>
