51fc3f34033195e113972c6b0b34cfe5ce64b655
Rakesh Tembhurne <rakesh@tembhurne.com>
2019-07-13 06:03:48 +0530

    Add quick navigation on home page
    Add quick temporary navigation to quickly navigate among pages.

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -3,13 +3,28 @@ import "./App.css";
 import AppContextProvider from "./AppContext";
 import UserStatus from "../UserStatus";
 import SmallContainer from "../../components/SmallContainer/SmallContainer";
+import SidebarList from "../../components/SidebarList/SidebarList";
+import { Link } from 'react-router-dom';
 
 function App() {
+  const quickLinks = [
+    <Link to="/signin">Sign In</Link>,
+    <Link to="/signup">Sign Up</Link>,
+    <Link to="/workspace">Workspace</Link>,
+    <Link to="/workspace/new">Create New Workspace</Link>,
+  ];
   return (
     <div className="App">
       <AppContextProvider>
         <SmallContainer>
           <UserStatus />
+          <br />
+          <div className="has-text-left">
+            <SidebarList 
+              heading="Quick Navigation"
+              list={quickLinks}
+            />
+          </div>
         </SmallContainer>
       </AppContextProvider>
     </div>
