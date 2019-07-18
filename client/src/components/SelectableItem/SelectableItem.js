53d613cfc6da805be1e00fc89014e4699c3622e0
Radheshyam Etikala <radhey1851@gmail.com>
2019-07-18 07:39:16 +0530

    Refactor selectableItem to change color on selection

--- client/src/components/SelectableItem/SelectableItem.js
+++ client/src/components/SelectableItem/SelectableItem.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 
 import Container from '../Container/Container';
 import './SelectableItem.css';
 
 const SelectableItem = (props) => {
-  const { handleChange, content, intialState } = props;
+  const { handleChange, content } = props;
+
+  const itemRef = useRef(null);
+
+  const handleToggle = (e) => {
+    const itemElement = itemRef.current;
+    itemElement.classList.toggle('item--selected');
+    if (handleChange) {
+      handleChange();
+    }
+  };
 
   return (
-    <div className="box item">
-      <input
-        type="checkbox"
-        defaultChecked={intialState}
-        onChange={handleChange}
-      />
+    <div ref={itemRef} className="box item">
+      <input type="checkbox" onChange={handleToggle} />
       <span className="item__content">{content}</span>
     </div>
   );
