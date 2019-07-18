cdd091f09b2a6d8a9bd9afdda626c4821ef54170
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-19 03:09:24 +0530

    Adds Nav Bar Adds Update Profile Page

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -3,27 +3,37 @@ import Form from '../../components/Form/Form'
 import { useAppContext } from '../../containers/App/AppContext'
 import { schema, inputs } from "./Schema";
 import { handleSubmit } from "./utils";
+import { Modal } from 'react-bulma-components';
+
 
 
 const Profile = (props) => {
 
     const { loginStatus, dispatch } = useAppContext();
 
-    console.log(loginStatus);
-
     return (
-        <div>
-            <Form
-                {...props}
-                handleSubmit={handleSubmit}
-                schema={schema}
-                inputs={inputs}
-                dispatch={dispatch}
-                submitLabel="Save Changes"
-                initialData={loginStatus.user}
-                alwaysEnableButton={true}
-            />
-        </div>
+        <Modal {...props}>
+            <Modal.Card>
+                <Modal.Card.Head onClose={props.onClose}>
+                    <Modal.Card.Title>Edit Profile</Modal.Card.Title>
+                </Modal.Card.Head>
+                <Modal.Card.Body {...props.modal}>
+                    <div>
+                        <Form
+                            {...props}
+                            handleSubmit={handleSubmit}
+                            schema={schema}
+                            inputs={inputs}
+                            dispatch={dispatch}
+                            submitLabel="Save Changes"
+                            initialData={loginStatus.user}
+                            alwaysEnableButton={true}
+                        />
+                    </div>
+                </Modal.Card.Body>
+            </Modal.Card>
+        </Modal>
+
     )
 }
 
