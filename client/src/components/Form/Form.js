0e250d29b61e6fb53ca9d1dbb3c5ad644f44b05f
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-19 23:56:18 +0530

    Update UI

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SmallContainer from '../SmallContainer/SmallContainer';
 import InputField from '../InputField/InputField';
 import {
   getError,
