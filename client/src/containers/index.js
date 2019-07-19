7e9b14e6a2f830dd3ba20c11409a1b81a996e9db
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-19 06:33:18 +0530

    (Feat): Adds Channel Members and Profile Panel

--- client/src/containers/index.js
+++ client/src/containers/index.js
@@ -3,3 +3,5 @@ export { default as SignUp } from './SignUp/SignUp';
 export { default as Workspaces } from './Workspaces/Workspaces';
 export { default as Thread } from './Thread/Thread';
 export { default as Profile } from './Profile/Profile';
+export { default as ViewProfile } from './Profile/ViewProfile';
+export { default as ChannelMembers } from './ChannelMembers/ChannelMembers';
