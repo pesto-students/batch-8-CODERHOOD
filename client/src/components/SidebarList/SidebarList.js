29b02c2e9b8907525a525f5be31b53ff2334db6d
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-22 01:20:19 +0530

    Add Channel Functionality

--- client/src/components/SidebarList/SidebarList.js
+++ client/src/components/SidebarList/SidebarList.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './SidebarList.css';
 
-const SidebarList = ({heading, action, headingTag = 'h4', list = [], ...props}) => {
+const SidebarList = ({ heading, action, headingTag = 'h4', list = [], actionClicked, ...props }) => {
   const HTag = headingTag;
-  const actionContent = action ? <span>{action}</span> : '';
-  const headingContent = heading 
-    ? <HTag>{heading} {actionContent}</HTag> 
+  const actionContent = action ? <span onClick={() => { actionClicked() }}>{action}</span> : '';
+  const headingContent = heading
+    ? <HTag>{heading} {actionContent}</HTag>
     : <HTag>{actionContent}</HTag>;
 
   return (
     <div className="content">
       {headingContent}
       <ul {...props}>
-        {list.map(ListItem => <li key={ListItem}>{ListItem}</li> )}
+        {list.map(ListItem => <li key={ListItem}>{ListItem}</li>)}
       </ul>
     </div>
   );
