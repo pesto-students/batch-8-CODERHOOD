2dab40b082def4d22e5b450e685679b6a478301d
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-24 04:57:03 +0530

    Remove member, people list issue

--- client/src/components/SmallContainer/SmallContainer.js
+++ client/src/components/SmallContainer/SmallContainer.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Container from '../Container/Container';
+import React from "react";
+import Container from "../Container/Container";
 
-const SmallContainer = (props) => {
+const SmallContainer = props => {
   return (
     <Container>
       <div className="columns is-centered">
         <div className="column is-full-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
-          {props.children}  
+          {props.children}
         </div>
       </div>
     </Container>
