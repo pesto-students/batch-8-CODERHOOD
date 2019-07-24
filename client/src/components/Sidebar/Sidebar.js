ac3d1c7dc211d7fbe60121ab506b6646c0378770
sourabhmodi20051 <sourabhmodi20051@gmail.com>
2019-07-25 02:59:45 +0530

    CSS and Image Changes

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -7,7 +7,8 @@ const Sidebar = ({ width = 2, ...props }) => {
     <div
       className={className}
       style={{
-        background: '#3e103f',
+        // background: '#3e103f',
+        background: '#343d4b',
         minHeight: '100%',
         paddingTop: '2.4em',
         paddingBottom: '0px'
